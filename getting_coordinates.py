265643841d1e9ff5578314e08dddd15e8f9ecea1
HARISREE SURESH <getharisree@outlook.com>
2023-03-29 19:55:07 +0530

    Update getting_coordinates.py

--- getting_coordinates.py
+++ getting_coordinates.py
@@ -1,1 +1,33 @@
+import cv2
+import numpy as np
+import time
+
+##open cv DNN
+net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights","dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(320,320),scale = 1/255)
+## capture the image
+cap = cv2.VideoCapture(0)
+
+while True:
+    ## getting the frame
+    success,frame = cap.read()
+    ## object detection
+
+    (class_ids,scores,bboxs) = model.detect(frame)
+    for class_id,score,bbox in zip(class_ids,scores,bboxs):
+        x,y,w,h = bbox
+        print(x,y,w,h)
+    #print(class_ids,scores,bboxs)
+
+
+
+
+
+
+    cv2.imshow("harvis",frame)
+    cv2.waitKey(1)
+
+
+
 
