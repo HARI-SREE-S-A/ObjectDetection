6481eb04f5dcc9ee92bd3a407623fb172db5bfd1
HARISREE SURESH <getharisree@outlook.com>
2023-04-11 21:32:59 +0530

    Update Final_working.py

--- Final_working.py
+++ Final_working.py
@@ -1,1 +1,8 @@
+rom ultralytics import YOLO
 
+
+model = YOLO("yolov8m_custom.pt")
+
+model.train(data="data_custom.yaml",batch=8 , imgsz=640, epochs=100, workers=1)
+def pred():
+    model.predict(source=0,show=True,save=True,con=0.5)
